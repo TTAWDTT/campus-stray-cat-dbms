0eb4215727bf7b987b43927c3782e1a70db1b29f
Xu Qianshun <2043794535@qq.com>
2026-07-30 17:23:05 +0800

    新增猫咪摘要查询模型

--- src/CampusStrayCatSystem.Data/ICatRepository.cs
+++ src/CampusStrayCatSystem.Data/ICatRepository.cs
@@ -1,9 +1,11 @@
 using CampusStrayCatSystem.Models;
 
-namespace CampusStrayCatSystem.Data
-{
-    public interface ICatRepository
-    {
+namespace CampusStrayCatSystem.Data {
+    public interface ICatRepository {
         Task<bool> Exists(string catId);
+        Task<IEnumerable<CatSummary>> GetAllAsync(string? mainAreaId = null,
+                                                  string? lifeStatus = null,
+                                                  string? archiveStatus = null);
+        Task<CatSummary?> GetByIdAsync(string catId);
     }
 }
