a74d929f968cb8f8d83ea225b0d03dafe737d23f
fanyu <2452750@tongji.edu.cn>
2026-08-11 14:09:40 +0800

    删除登陆注释

--- src/CampusStrayCatSystem.App/src/app/App.tsx
+++ src/CampusStrayCatSystem.App/src/app/App.tsx
@@ -36,11 +36,10 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/preview/shared" element={<SharedComponentsPreview />} />
-         { /*
+         
             <Route element={<AuthLayout />}>
               <Route path="/login" element={<LoginPage />} />
             </Route>
-*/}
         <Route element={<RouteGuard />}>
           <Route element={<MainLayout />}>
             <Route index element={<DashboardPage />} />
